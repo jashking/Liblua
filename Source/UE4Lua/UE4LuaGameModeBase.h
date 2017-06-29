cacb2ed8652ef138d01f2b4e1c86d0fcb1cd15f4
Jash <newzeadev@gmail.com>
2017-06-29 17:10:57 +0800

    add lua-5.3.4

---
+++ Source/UE4Lua/UE4LuaGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4LuaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4LUA_API AUE4LuaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
