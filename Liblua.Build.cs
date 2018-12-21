83189f3e512a74db6d63fe3390f23013e8b4eb76
Jash <newzeadev@gmail.com>
2018-12-21 11:12:52 +0800

    lua_tmpnam use posix version on android

--- Liblua.Build.cs
+++ Liblua.Build.cs
@@ -7,14 +7,20 @@ public class Liblua : ModuleRules
 	public Liblua(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		bEnableShadowVariableWarnings = false;
 		bEnableUndefinedIdentifierWarnings = false;
 
 		PublicDefinitions.Add("WITH_LIBLUA=1");
+
+		//if (Target.Platform == UnrealTargetPlatform.Android || Target.Platform == UnrealTargetPlatform.Linux ||
+		//	Target.Platform == UnrealTargetPlatform.Mac || Target.Platform == UnrealTargetPlatform.IOS)
+		//{
+		//	PrivateDefinitions.Add("LUA_USE_POSIX");
+		//}
 	}
 }
